14e2137dc52973b8f7cf619be02a783ed8faa1c0
Anthony Weihao Ke <keanthon@umich.edu>
2022-02-23 15:35:30 -0500

    Reconfigured backend. Refactored styling code. Added text verification functionality

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -1,18 +1,30 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:moood/widgets/text_field_input.dart';
+import 'package:moood/widgets/input_decoration.dart';
 import 'signup_screen.dart';
 
-class LoginScreen extends StatefulWidget {
-  const LoginScreen({Key? key}) : super(key: key);
+// Form widget
+class MyCustomForm extends StatefulWidget {
+  const MyCustomForm({Key? key}) : super(key: key);
 
   @override
-  _LoginScreenState createState() => _LoginScreenState();
+  MyCustomFormState createState() {
+    return MyCustomFormState();
+  }
 }
 
-class _LoginScreenState extends State<LoginScreen> {
+// Define a corresponding State class.
+// This class holds data related to the form.
+class MyCustomFormState extends State<MyCustomForm> {
+  // Create a global key that uniquely identifies the Form widget
+  // and allows validation of the form.
+  //
+  // Note: This is a `GlobalKey<FormState>`,
+  // not a GlobalKey<MyCustomFormState>.
+  final _formKey = GlobalKey<FormState>();
   final TextEditingController _emailCont = TextEditingController();
   final TextEditingController _passwordCont = TextEditingController();
+
 
   @override
   void dispose() {
@@ -23,84 +35,123 @@ class _LoginScreenState extends State<LoginScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
+    // Build a Form widget using the _formKey created above.
+    requireFunc(value) {
+      if (value == null || value.isEmpty) {
+        return 'Required';
+      }
+      return null;
+    }
 
+    return Form(
+      key: _formKey,
+      child: Column(
+        children: [
+
+          CircleAvatar(
+            radius: 100,
+            backgroundImage: AssetImage('assets/images/logo.jpg'),
+          ),
+          const SizedBox(height: 64,),
+
+          TextFormField(
+            validator: requireFunc,
+            controller: _emailCont,
+            keyboardType: TextInputType.emailAddress,
+            obscureText: false,
+            decoration: TextInputDecoration(
+                'Enter your email',
+                Colors.redAccent[100]).decorate(),
+          ),
+          const SizedBox(height: 10,),
+
+          TextFormField(
+            controller: _passwordCont,
+            keyboardType: TextInputType.text,
+            obscureText: true,
+            decoration: TextInputDecoration(
+                'Password',
+                Colors.redAccent[100]).decorate(),
+            validator: requireFunc,
+          ),
+          const SizedBox(height: 10,),
+
+          ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                primary: Colors.deepPurpleAccent[100],
+                fixedSize: Size(300, 50),
+              ),
+              onPressed: (){
+                if (_formKey.currentState!.validate())
+                  print("bitch");
+              },
+              child: const Text('Submit')
+          ),
+
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Container(
+                child: const Text(
+                  'Don\'t have an account?',
+                  style: TextStyle(color: Colors.grey),
+                ),
+                padding: const EdgeInsets.symmetric(vertical: 8),
+              ),
+              GestureDetector(
+                onTap: () => Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => const SignupScreen(),
+                  ),
+                ) ,
+                child: Container(
+                  child: const Text(
+                    ' Signup.',
+                    style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        color: Colors.grey
+                    ),
+                  ),
+                  padding: const EdgeInsets.symmetric(vertical: 8),
+                ),
+              ),
+            ],
+          ),
+
+          //signup
+
+
+        ],
+      ),
+    );
+  }
+}
+
+class LoginScreen extends StatefulWidget {
+  const LoginScreen({Key? key}) : super(key: key);
+
+  @override
+  _LoginScreenState createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+
+  @override
+  Widget build(BuildContext context) {
+
+    return Scaffold(
       body: SafeArea(
         child: Center(
           child: SingleChildScrollView(
             reverse: true,
             padding: EdgeInsets.all(32),
-            child: Column(
-              children: [
-
-                CircleAvatar(
-                  radius: 100,
-                  backgroundImage: AssetImage('assets/images/logo.jpg'),
-                ),
-                const SizedBox(height: 64,),
-                TextFieldInput(
-                  textEditingController: _emailCont,
-                  textInputType: TextInputType.emailAddress,
-                  hintText: 'Enter your email',
-                  backgroundColor: Colors.redAccent[100],
-                ),
-                const SizedBox(height: 10,),
-                TextFieldInput(
-                  textEditingController: _passwordCont,
-                  textInputType: TextInputType.emailAddress,
-                  hintText: 'Password',
-                  backgroundColor: Colors.redAccent[100],
-                  isPass: true,
-                ),
-                const SizedBox(height: 10,),
-                ElevatedButton(
-                  style: ElevatedButton.styleFrom(
-                    primary: Colors.deepPurpleAccent[100],
-                    fixedSize: Size(300, 50),
-                  ),
-                  onPressed: (){},
-                  child: const Text('Login')
-                ),
-
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Container(
-                      child: const Text(
-                        'Dont have an account?',
-                        style: TextStyle(color: Colors.grey),
-                      ),
-                      padding: const EdgeInsets.symmetric(vertical: 8),
-                    ),
-                    GestureDetector(
-                      onTap: () => Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                          builder: (context) => const SignupScreen(),
-                        ),
-                      ) ,
-                      child: Container(
-                        child: const Text(
-                          ' Signup.',
-                          style: TextStyle(
-                            fontWeight: FontWeight.bold,
-                            color: Colors.grey
-                          ),
-                        ),
-                        padding: const EdgeInsets.symmetric(vertical: 8),
-                      ),
-                    ),
-                  ],
-                ),
-
-                //signup
-
-
-              ],
-            ),
+            child: const MyCustomForm(),
           ),
         ),
       ),
     );
   }
 }
+
+
