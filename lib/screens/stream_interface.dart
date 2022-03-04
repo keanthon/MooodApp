5fbcf571eb4412c7125024ec109322b60198ab36
Michael Ni <nimic@umich.edu>
2022-03-04 15:21:59 -0500

    array of statuses

--- lib/screens/stream_interface.dart
+++ lib/screens/stream_interface.dart
@@ -1,3 +1,5 @@
+import 'dart:convert';
+
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
@@ -24,6 +26,7 @@ class StreamInterfaceState extends State<StreamInterface> {
   final FirebaseFirestore _firestore = FirebaseFirestore.instance;
   final TextEditingController statusController = TextEditingController();
   final maxStatusCount = 20;
+  List<String> userStatuses = [];
 
   StreamInterfaceState({Key? key, required this.user}) {
     List<String>? split = user.displayName?.split(" ");
@@ -33,14 +36,15 @@ class StreamInterfaceState extends State<StreamInterface> {
 
   String firstName = "";
   String lastName = "";
-  String userStatus = "";
 
   Future<void> getUserStatus() async {
-    _firestore.collection("users").doc(user.uid).get().then((value) {
-      setState(() {
-        userStatus = value.get("status");
-      });
-    });
+    _firestore.collection("users").doc(user.uid).get()
+        .then((value) {
+          setState(() {
+            userStatuses = (json.decode(value.get("statuses")) as List<dynamic>).cast<String>();
+          });
+        })
+        .catchError((error) => print(error));
   }
 
   @override
@@ -76,106 +80,99 @@ class StreamInterfaceState extends State<StreamInterface> {
             child: IconButton(
               icon: const Icon(Icons.account_circle, color: primaryColor, size: 30),
               onPressed: () {
-                Route route = MaterialPageRoute(builder: (context) => Profile(user: appUser(firstName, lastName, userStatus)));
+                Route route = MaterialPageRoute(builder: (context) => Profile(user: appUser(firstName, lastName, userStatuses)));
                 Navigator.push(context, route);
               },
             )
           ),
         ],
       ),
-      body: Center (
-          child: Column (
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: <Widget>[
-              const Padding(
-                padding: EdgeInsets.symmetric(vertical: 10),
-              ),
-              Text(
-                "Hi ${firstName}! How are you doing?",
-                style: TextStyle(
-                  color: secondaryColor,
-                  fontSize: 24,
-                )
-              ),
-              // these are suggested statuses
-              Container(
-                  decoration: BoxDecoration(
-                      borderRadius: BorderRadius.all(Radius.circular(10.0))),
-                  margin: EdgeInsets.all(5),
-                  child: Row (
-                    mainAxisAlignment: MainAxisAlignment.spaceAround,
-                    children: <Widget>[
-                      redCenteredContainer("good"),
-                      redCenteredContainer("bad"),
-                      redCenteredContainer("at the UGLI"),
-                      redCenteredContainer("meh"),
-                      redCenteredContainer("thoughtful")
-                    ]
-                  )
-              ),
-              Container(
-                margin: EdgeInsets.only(left: 10, top: 10, right: 10, bottom: 0),
-                child: TextFormField(
-                  validator: requireFunc,
-                  controller: statusController,
-                  keyboardType: TextInputType.text,
-                  obscureText: false,
-                  maxLength: maxStatusCount,
-                  buildCounter: (_, {required currentLength, maxLength, required isFocused}) => Padding(
-                    padding: const EdgeInsets.only(left: 16.0),
-                    child: Text(
-                        (maxLength! - currentLength).toString() + " characters left!!",
-                        style: TextStyle(
-                          color: secondaryColor,
-                          fontSize: 18
+      body: SingleChildScrollView(
+        child: Center (
+              child: Column (
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: <Widget>[
+                  const Padding(
+                    padding: EdgeInsets.symmetric(vertical: 10),
+                  ),
+                  Text(
+                    "Hi ${firstName}! How are you doing?",
+                    style: TextStyle(
+                      color: secondaryColor,
+                      fontSize: 24,
+                    )
+                  ),
+                  // these are suggested statuses
+                  Container(
+                      decoration: BoxDecoration(
+                          borderRadius: BorderRadius.all(Radius.circular(10.0))),
+                      margin: EdgeInsets.all(5),
+                      child: Row (
+                        mainAxisAlignment: MainAxisAlignment.spaceAround,
+                        children: <Widget>[
+                          redCenteredContainer("good"),
+                          redCenteredContainer("bad"),
+                          redCenteredContainer("at the UGLI"),
+                          redCenteredContainer("meh"),
+                          redCenteredContainer("thoughtful")
+                        ]
+                      )
+                  ),
+                  Container(
+                    margin: EdgeInsets.only(left: 10, top: 10, right: 10, bottom: 0),
+                    child: TextFormField(
+                      validator: requireFunc,
+                      controller: statusController,
+                      keyboardType: TextInputType.text,
+                      obscureText: false,
+                      maxLength: maxStatusCount,
+                      buildCounter: (_, {required currentLength, maxLength, required isFocused}) => Padding(
+                        padding: const EdgeInsets.only(left: 16.0),
+                        child: Text(
+                            (maxLength! - currentLength).toString() + " characters left!!",
+                            style: TextStyle(
+                              color: secondaryColor,
+                              fontSize: 18
+                            ),
                         ),
+                      ),
+                      decoration: TextInputDecoration(
+                          '... What a mood',
+                          Colors.redAccent[100]).decorate(),
                     ),
                   ),
-                  decoration: TextInputDecoration(
-                      '... What a mood',
-                      Colors.redAccent[100]).decorate(),
-                ),
-              ),
-              // FIXME bug with text form not working when going back on stream and retrying
-              Material(
-                child: InkWell(
-                  onTap: () {
-                    _firestore.collection("users").doc(user.uid).update({"status": statusController.text});
-                    setState(() {
-                      userStatus = statusController.text;
-                      statusController.text = "";
-                    });
-                  },
-                  splashColor: secondaryColor,
-                  child: Container(
-                      padding: EdgeInsets.all(10),
-                      child: const Text(
-                        "Post",
-                        style: TextStyle(
-                            fontSize: 18
-                        ),
-                      )
+                  // FIXME friends
+                  Material(
+                    child: InkWell(
+                      onTap: () {
+                        userStatuses.add(statusController.text);
+                        String userStatusJSON = json.encode(userStatuses);
+
+                        _firestore.collection("users").doc(user.uid).update({"statuses": userStatusJSON})
+                          .catchError((error) {
+                          _firestore.collection("users").doc(user.uid).update({"statuses": userStatusJSON});
+                        });
+
+                        setState(() {
+                          statusController.text = "";
+                        });
+                      },
+                      splashColor: secondaryColor,
+                      child: Container(
+                          padding: EdgeInsets.all(10),
+                          child: const Text(
+                            "Post",
+                            style: TextStyle(
+                                fontSize: 18
+                            ),
+                          )
+                      ),
+                    )
                   ),
-                )
-              ),
-              // previously saved status
-              Container(
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.all(Radius.circular(10.0)),
-                    color: tertiaryColor,
-                  ),
-                  padding: EdgeInsets.all(10),
-                  margin: EdgeInsets.all(20),
-                  child: Text(
-                      userStatus,
-                      style: TextStyle(
-                        fontSize: 18,
-                        color: primaryColor,
-                      )
-                  )
-              ),
-            ]
-        )
+                  generate_posts(userStatuses),
+                ]
+            )
+        ),
       )
     );
   }
