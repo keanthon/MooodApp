9aef5e1a9c90a794959ca28f22c1685eb1276952
Anthony Weihao Ke <keanthon@umich.edu>
2022-03-16 05:37:14 -0400

    Setup framework for calendar

---
+++ lib/screens/calendar_screen.dart
@@ -0,0 +1,72 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+import 'package:table_calendar/table_calendar.dart';
+
+class CalendarScreen extends StatefulWidget {
+  const CalendarScreen({Key? key}) : super(key: key);
+
+  @override
+  State<CalendarScreen> createState() => _CalendarScreenState();
+}
+
+class _CalendarScreenState extends State<CalendarScreen> {
+  late final ValueNotifier<List<QuerySnapshot>> _selectedEvents;
+  CalendarFormat _calendarFormat = CalendarFormat.month;
+  DateTime _focusedDay = DateTime.now();
+  DateTime? _selectedDay;
+
+  @override
+  void initState() {
+    super.initState();
+
+    _selectedDay = _focusedDay;
+    // _selectedEvents = ValueNotifier(_getEventsForDay(_selectedDay!));
+  }
+
+  @override
+  void dispose() {
+    _selectedEvents.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return TableCalendar(
+      firstDay: DateTime.utc(2021, 10, 16),
+      lastDay: DateTime.now().add(const Duration(hours: 1)),
+      focusedDay: _focusedDay,
+      calendarFormat: _calendarFormat,
+      selectedDayPredicate: (day) {
+        // Use `selectedDayPredicate` to determine which day is currently selected.
+        // If this returns true, then `day` will be marked as selected.
+
+        // Using `isSameDay` is recommended to disregard
+        // the time-part of compared DateTime objects.
+        return isSameDay(_selectedDay, day);
+      },
+      onDaySelected: (selectedDay, focusedDay) {
+        if (!isSameDay(_selectedDay, selectedDay)) {
+          // Call `setState()` when updating the selected day
+          setState(() {
+            _selectedDay = selectedDay;
+            _focusedDay = focusedDay;
+          });
+
+
+        }
+      },
+      onFormatChanged: (format) {
+        if (_calendarFormat != format) {
+          // Call `setState()` when updating calendar format
+          setState(() {
+            _calendarFormat = format;
+          });
+        }
+      },
+      onPageChanged: (focusedDay) {
+        // No need to call `setState()` here
+        _focusedDay = focusedDay;
+      },
+    );
+  }
+}
--- lib/screens/my_posts.dart
+++ lib/screens/my_posts.dart
@@ -1,21 +1,47 @@
 import 'package:flutter/material.dart';
 import 'package:moood/components/fetch_posts.dart';
+import 'package:moood/screens/calendar_screen.dart';
 import 'package:provider/provider.dart';
 
 import '../models/user_class.dart';
 import '../providers/user_provider.dart';
 
 class MyPosts extends StatefulWidget {
-  const MyPosts({Key? key,}) : super(key: key);
+  const MyPosts({
+    Key? key,
+  }) : super(key: key);
 
   @override
   State<MyPosts> createState() => _MyPostsState();
 }
 
 class _MyPostsState extends State<MyPosts> {
+  final tab = TabBar(tabs: <Tab>[
+    Tab(icon: Icon(Icons.calendar_month)),
+    Tab(icon: Icon(Icons.list_alt)),
+  ]);
+
   @override
   Widget build(BuildContext context) {
     UserClass? user = Provider.of<UserProvider>(context).getUser;
-    return FetchPosts(uid: user!.uid, feedOrPost: "posts",);
+    return SafeArea(
+      child: DefaultTabController(
+        length: 2,
+        child: Scaffold(
+          appBar: AppBar(
+            bottom: tab,
+          ),
+          body: TabBarView(
+            children: [
+              CalendarScreen(),
+              FetchPosts(
+              uid: user!.uid,
+              feedOrPost: "posts",
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
