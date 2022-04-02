ea8279605d2ee7b4f471c309f4cdd3c93ceb20cd
Anthony Weihao Ke <keanthon@umich.edu>
2022-04-02 10:57:47 -0400

    change photo done

--- lib/screens/profile.dart
+++ lib/screens/profile.dart
@@ -1,5 +1,8 @@
+import 'dart:typed_data';
+
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
 import 'package:moood/components/post_card.dart';
 import 'package:moood/models/user_class.dart';
 import 'package:moood/screens/login_screen.dart';
@@ -10,11 +13,24 @@ import '../utils/colors_styles.dart';
 import '../utils/helper_functions.dart';
 import 'my_posts.dart';
 
-class Profile extends StatelessWidget {
+class Profile extends StatefulWidget {
 
   Profile({Key? key});
 
+  @override
+  State<Profile> createState() => _ProfileState();
+}
 
+class _ProfileState extends State<Profile> {
+  Uint8List? _image;
+
+  selectImage() async {
+    Uint8List im = await pickImage(ImageSource.gallery);
+    // set state because we need to display the image we selected on the circle avatar
+    setState(() {
+      _image = im;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -49,10 +65,34 @@ class Profile extends StatelessWidget {
                 Padding(
                   padding: EdgeInsets.only(top: 10),
                 ),
-                CircleAvatar(
-                  radius: 100,
-                  backgroundImage: NetworkImage(user.photoUrl),
+                Stack(
+                  children: [
+                    _image != null
+                        ? CircleAvatar(
+                      radius: 100,
+                      backgroundImage: MemoryImage(_image!),
+                      backgroundColor: Colors.white,
+                    )
+                        : CircleAvatar(
+                      radius: 100,
+                      backgroundImage: NetworkImage(user.photoUrl),
+                    ),
+                    Positioned(
+                      bottom: -10,
+                      left: 80,
+                      child: IconButton(
+                        onPressed: () async {
+                          await selectImage();
+                          await AuthMethods().uploadImageToStorage('profilePics', _image!, false);
+
+                        },
+                        icon: const Icon(Icons.add_a_photo),
+                      ),
+                    )
+                  ],
                 ),
+
+
                 Text(
                     "Hi ${user.firstName} ${user.lastName}!\n#${getShortUID(user.uid)}",
                     style: TextStyle(
